f7220ea8c100b56e869c7cf818b769abbc8687ed
Eugene Rodionov <git@theaqua.im>
2015-07-30 23:57:38 +0300

    feat: playing with redux

--- src/app.js
+++ src/app.js
@@ -2,5 +2,10 @@ import 'babel-core/polyfill';
 
 import React from 'react';
 import Landing from 'views/pages/landing';
+import store from 'store';
 
 React.render(<Landing />, document.getElementById('app'));
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import counter from 'reducers/counter';
+
+let store = createStore(counter);
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
+
+export default store;
