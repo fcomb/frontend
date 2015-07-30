f3255098d080a08f180c1e54e00903d868e2ae5d
Eugene Rodionov <git@theaqua.im>
2015-07-30 19:16:38 +0300

    fix: import babel polyfills

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
+import 'babel-core/polyfill';
+
 import React from 'react';
-import Polyfills from 'babel-core/polyfill';
 import Landing from 'views/pages/landing';
 
 React.render(<Landing />, document.getElementById('app'));
