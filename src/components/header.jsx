ddba311090cc4634b8f63ea231d80414f8bcb476
Eugene Rodionov <git@theaqua.im>
2015-08-01 19:35:33 +0300

    chore: add links to navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -18,8 +18,12 @@ class Header extends Component {
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-              <li><a href="#">Link</a></li>
+              <li>
+                <Link to="/explore" activeClassName="active">Explore</Link>
+              </li>
+              <li>
+                <Link to="/about" activeClassName="active">About</Link>
+              </li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li><a href="#">Link</a></li>
