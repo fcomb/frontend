1b75130197e71094e2e99a7b49f9caa4cb8f8115
Eugene Rodionov <git@theaqua.im>
2015-11-11 12:07:43 +0300

    feat: add sign up

--- src/containers/user/sign_up.jsx
+++ src/containers/user/sign_up.jsx
@@ -1,3 +1,58 @@
-export default () => {
-  return null;
-};
+import React, { Component } from 'react';
+import UI from 'components/ui';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as AuthActions from 'actions/auth';
+
+const {
+  Grid, Grid: { Row, Col },
+  Form, Form: { Input, Group},
+  Button,
+} = UI;
+const styles = {};
+
+class SignUpContainer extends Component {
+  handleOnSubmit(e, data) {
+    e.preventDefault();
+
+    this.props.actions.signUp(data);
+  }
+
+  render() {
+    return (
+      <Grid.Container>
+        <Row>
+          <Col sm="4" smOffset="4">
+            <div className={styles.panel}>
+              <h3 className={styles.textCenter}>Sign Up {this.props.state.auth.inProccess && <span>LOADING</span>}</h3>
+
+              <Form.Container onSubmit={::this.handleOnSubmit}>
+                <Group size="lg">
+                  <Input name="email" type="email" placeholder="E-mail" block />
+                </Group>
+
+                <Group size="lg">
+                  <Input name="password" type="password" placeholder="Password" block />
+                </Group>
+
+                <Button type="submit" kind="primary" size="lg" block>Sign In</Button>
+                <p>By signing up, you agree to the Terms of Service</p>
+              </Form.Container>
+            </div>
+          </Col>
+        </Row>
+      </Grid.Container>
+    );
+  }
+}
+
+const mapState = ({ auth }) => ({
+  state: { auth },
+});
+
+const mapDispatch = (dispatch) => ({
+  actions: bindActionCreators(AuthActions, dispatch),
+});
+
+export default connect(mapState, mapDispatch)(SignUpContainer);
