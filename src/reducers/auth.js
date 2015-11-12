603ce91c7c1daedd18c53fa833693d1ec53ca0cb
Eugene Rodionov <git@theaqua.im>
2015-11-12 23:49:13 +0300

    fix: reduce auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -4,41 +4,27 @@ import * as types from 'constants/auth';
 const initialState = {};
 
 const auth = handleActions({
-  [types.SIGN_UP]: (state) => ({
-    ...state,
+  [types.SIGN_UP]: () => ({
     inProccess: true,
   }),
 
   [types.SIGN_UP_SUCCEEDED]: (state, { id }) => ({
-    ...state,
-    inProccess: false,
-
     id,
   }),
 
   [types.SIGN_UP_FAILED]: (state, { errors }) => ({
-    ...state,
-    inProccess: false,
-
     errors,
   }),
 
-  [types.SIGN_IN]: (state) => ({
-    ...state,
+  [types.SIGN_IN]: () => ({
     inProccess: true,
   }),
 
   [types.SIGN_IN_SUCCEEDED]: (state, { token }) => ({
-    ...state,
-    inProccess: false,
-
     token,
   }),
 
   [types.SIGN_IN_FAILED]: (state, { errors }) => ({
-    ...state,
-    inProccess: false,
-
     errors,
   }),
 }, initialState);
