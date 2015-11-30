914369d5357143c031b8a4325ddb3cfb45e62945
Eugene Rodionov <git@theaqua.im>
2015-11-30 12:18:45 +0300

    fix: clean up

--- src/containers/containers/list.jsx
+++ src/containers/containers/list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import UI from 'components/ui';
 const { Grid, Table } = UI;
 
@@ -12,29 +13,21 @@ export default () => (
           <Table.Row>
             <Table.Heading width="1%">&nbsp;</Table.Heading>
             <Table.Heading>Name</Table.Heading>
-            <Table.Heading width="15%">Internal IP</Table.Heading>
-            <Table.Heading width="15%">External IP</Table.Heading>
+            <Table.Heading width="15%">IP Address</Table.Heading>
             <Table.Heading width="7%">Memory</Table.Heading>
             <Table.Heading width="7%">Disk</Table.Heading>
           </Table.Row>
-          <Table.Row>
-            <Table.Cell>ok</Table.Cell>
-            <Table.Cell>name</Table.Cell>
-            <Table.Cell>127.0.0.1</Table.Cell>
-            <Table.Cell>234.255.193.251</Table.Cell>
-            <Table.Cell>1 GB</Table.Cell>
-            <Table.Cell>20 GB</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>ok</Table.Cell>
-            <Table.Cell>name</Table.Cell>
-            <Table.Cell>127.0.0.1</Table.Cell>
-            <Table.Cell>
-              <UI.Button kind="primary" block>Order</UI.Button>
-            </Table.Cell>
-            <Table.Cell>1 GB</Table.Cell>
-            <Table.Cell>20 GB</Table.Cell>
-          </Table.Row>
+          {[{ id: 1, name: `first` }, { id: 2, name: `second` }].map(({ id, name }) => (
+            <Table.Row>
+              <Table.Cell>ok</Table.Cell>
+              <Table.Cell>
+                <Link to={`/containers/${id}`}>{name}</Link>
+              </Table.Cell>
+              <Table.Cell>234.255.193.251</Table.Cell>
+              <Table.Cell>1 GB</Table.Cell>
+              <Table.Cell>20 GB</Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Container>
       </Grid.Col>
     </Grid.Row>
